c509a2f6092a97b423910d65891d930c87184279
Krish patel <krishp2304@gmail.com>
2024-11-19 09:38:30 +0530

    change

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,18 +1,18 @@
 import Display from "../assets/icons_FEtask/Display.svg";
 import todo from "../assets/icons_FEtask/To-do.svg";
-import inprogress from "../assets/icons_FEtask/in-progress.svg"
-import Down from "../assets/icons_FEtask/down.svg"
-import backlog from "../assets/icons_FEtask/Backlog.svg"
-import done from "../assets/icons_FEtask/Done.svg"
-import cancelled from "../assets/icons_FEtask/Cancelled.svg"
-import threeDotMenu from "../assets/icons_FEtask/3 dot menu.svg"
+import inprogress from "../assets/icons_FEtask/in-progress.svg";
+import Down from "../assets/icons_FEtask/down.svg";
+import backlog from "../assets/icons_FEtask/Backlog.svg";
+import done from "../assets/icons_FEtask/Done.svg";
+import cancelled from "../assets/icons_FEtask/Cancelled.svg";
+import threeDotMenu from "../assets/icons_FEtask/3 dot menu.svg";
 import urgentGrey from "../assets/icons_FEtask/SVG - Urgent Priority grey.svg";
 import urgentColour  from "../assets/icons_FEtask/SVG - Urgent Priority colour.svg";
-import noPriority from "../assets/icons_FEtask/No-priority.svg"
-import lowPriority from "../assets/icons_FEtask/Img - Low Priority.svg"
-import midPriority from "../assets/icons_FEtask/Img - Medium Priority.svg"
-import highPriority from "../assets/icons_FEtask/Img - High Priority.svg"
-import add from "../assets/icons_FEtask/add.svg"
+import noPriority from "../assets/icons_FEtask/No-priority.svg";
+import lowPriority from "../assets/icons_FEtask/Img - Low Priority.svg";
+import midPriority from "../assets/icons_FEtask/Img - Medium Priority.svg";
+import highPriority from "../assets/icons_FEtask/Img - High Priority.svg";
+import add from "../assets/icons_FEtask/add.svg";
 
 
 export{
